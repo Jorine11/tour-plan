22067c131f8f08f379783363a06a151b8e03a4c3
Dmitriy Mineev <Seuin54@gmail.com>
2021-03-25 04:13:50 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -30,38 +30,6 @@ $(document).ready(function() {
     },
   });
 
-//   var fired = false;
-
-// window.addEventListener('scroll', () => {
-//     if (fired === false) {
-//         fired = true;
-        
-//         setTimeout(() => {
-// ymaps.ready(init);
-//       function init(){
-//           // Создание карты.
-//           var myMap = new ymaps.Map("map", {
-//               // Координаты центра карты.
-//               // Порядок по умолчанию: «широта, долгота».
-//               // Чтобы не определять координаты центра карты вручную,
-//               // воспользуйтесь инструментом Определение координат.
-//               center: [55.774054, 37.651793],
-//               // Уровень масштабирования. Допустимые значения:
-//               // от 0 (весь мир) до 19.
-//               zoom: 17
-//           });
-//           var myPlacemark = new ymaps.Placemark( [55.774054, 37.651793], {
-//             balloonContent: 'Россия, Москва, Каланчёвская улица, 21/40',
-//             hintContent:  'Hilton Moscow Leningradskaya'
-//           });
-//           myMap.geoObjects.add(myPlacemark);
-//         };
-//         }, 1000)
-//     }
-// });
-
-      
-
   var menuButton = document.querySelector('.menu-button');
   menuButton.addEventListener('click', function(){ 
     document.querySelector('.navbar-bottom').classList.toggle("navbar-bottom--visible")
@@ -72,11 +40,11 @@ $(document).ready(function() {
   var modalButton = $('[data-toggle=modal]');
   var closeButton = $('.modal__close');
   modalButton.on('click', openModal);
-
   closeButton.on('click', closeModal);
 
 
-  function openModal() {
+  function openModal(e) {
+    e.preventDefault();
     var modalOverlay = $('.modal__overlay');
     var modalDialog = $('.modal__dialog');
     modalOverlay.addClass('modal__overlay--visible');
@@ -84,14 +52,15 @@ $(document).ready(function() {
     $('body').addClass("fixed");
   };
 
-  function closeModal(event) {
-    event.preventDefault();
+  function closeModal(e) {
+    e.preventDefault();
     var modalOverlay = $('.modal__overlay');
     var modalDialog = $('.modal__dialog');
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
     $('body').removeClass("fixed");
   };
+
   $(document).keyup(function(e) {
     if (e.key === "Escape") {
       var modalOverlay = $('.modal__overlay');
